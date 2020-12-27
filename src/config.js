1a40c7e35513457fa6830f6d1f3e6066031a7424
Eman Curmi <emancurmi@gmail.com>
2020-12-27 01:28:49 -0500

    switch to production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    //API_ENDPOINT: 'https://guardian-ride-app.herokuapp.com/api/',
-    API_ENDPOINT: 'http://localhost:8000/api/',
+    API_ENDPOINT: 'https://guardian-ride-app.herokuapp.com/api/',
+    //API_ENDPOINT: 'http://localhost:8000/api/',
     API_TOKEN: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
     cookie_key : 'idcrypt'
 }
